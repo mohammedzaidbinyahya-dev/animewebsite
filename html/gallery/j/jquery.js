9d9e779e1172f4e034ca5ebba4af17060b668af3
mohammedzaidbinyahya-dev <mohammed.zaid.binyahya@gmail.com>
2025-09-16 21:44:24 +0300

    first commit

---
+++ html/gallery/j/jquery.js
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<VisualLightBox>
+    <Gallery version="2.5">
+        <Settings>
+            <AppName>WOW Slider</AppName>
+            <AppUrlS>WOWSlider.com</AppUrlS>
+            <AppVersion>9.0</AppVersion>
+            <AutoPlay>true</AutoPlay>
+            <AutoPlayVideo>false</AutoPlayVideo>
+            <Captions>true</Captions>
+            <CaptionsEffect>parallax</CaptionsEffect>
+            <Controls>true</Controls>
+            <FontSize>25</FontSize>
+            <FullScreen>false</FullScreen>
+            <GalleryName>wow</GalleryName>
+            <GallerySuffix>1</GallerySuffix>
+            <HtmlCharset>UTF-8</HtmlCharset>
+            <ImageAspectRatio>true</ImageAspectRatio>
+            <ImageEffect>rotate</ImageEffect>
+            <ImageFillColor>#000000</ImageFillColor>
+            <ImageHeight>720</ImageHeight>
+            <ImageQuality>90</ImageQuality>
+            <ImageShrink2>false</ImageShrink2>
+            <ImageStretch>true</ImageStretch>
+            <ImageTemplate>C:/Program Files (x86)/WOW Slider/templates/backgnd/puzzle/</ImageTemplate>
+            <ImageTemplateName>Puzzle</ImageTemplateName>
+            <ImageWidth>1280</ImageWidth>
+            <Images>true</Images>
+            <JoomlaType>2.5</JoomlaType>
+            <OneLoop>false</OneLoop>
+            <OrderRandom>false</OrderRandom>
+            <PlayPause>true</PlayPause>
+            <Preloader>false</Preloader>
+            <PublishIndex>wowslider.html</PublishIndex>
+            <PublishPreview>true</PublishPreview>
+            <PublishType>file</PublishType>
+            <RelativePath>false</RelativePath>
+            <ShowBullets>1</ShowBullets>
+            <ShowControls>false</ShowControls>
+            <ShowTooltips>1</ShowTooltips>
+            <SizeStyle>1</SizeStyle>
+            <SlideshowDelay>20</SlideshowDelay>
+            <SlideshowDuration>20</SlideshowDuration>
+            <SoundEnable>0</SoundEnable>
+            <SoundFileName></SoundFileName>
+            <StopOnHover>false</StopOnHover>
+            <SwipeSupport>2</SwipeSupport>
+            <TemplateColor>#000000</TemplateColor>
+            <TemplateFont>Lobster</TemplateFont>
+            <TemplateName>puzzle</TemplateName>
+            <ThumbAlign>bottom</ThumbAlign>
+            <ThumbFormat>jpg</ThumbFormat>
+            <ThumbHeight>48</ThumbHeight>
+            <ThumbQuality>90</ThumbQuality>
+            <ThumbWidth>0</ThumbWidth>
+            <Thumbnails>false</Thumbnails>
+            <TooltipPos>bottom</TooltipPos>
+            <noFrame>0</noFrame>
+        </Settings>
+        <Images RelativePath="false">
+            <Image>
+                <url>C:/Users/ha/Desktop/project 1/images/Gallery/Gallery1.jpg</url>
+                <title>Famous Anime</title>
+                <filesize>117184</filesize>
+            </Image>
+            <Image>
+                <url>C:/Users/ha/Desktop/project 1/images/Gallery/Gallery2.jpg</url>
+                <title>Naruto</title>
+                <filesize>100888</filesize>
+            </Image>
+            <Image>
+                <url>C:/Users/ha/Desktop/project 1/images/Gallery/Gallery3.jpg</url>
+                <title>Hunter x Hunter</title>
+                <filesize>100604</filesize>
+            </Image>
+            <Image>
+                <url>C:/Users/ha/Desktop/project 1/images/Gallery/Gallery4.jpg</url>
+                <title>One Piece</title>
+                <filesize>34252</filesize>
+            </Image>
+            <Image>
+                <url>C:/Users/ha/Desktop/project 1/images/Gallery/Gallery5.jpg</url>
+                <title>Attack on Titan</title>
+                <filesize>37132</filesize>
+            </Image>
+            <Image>
+                <url>C:/Users/ha/Desktop/project 1/images/Gallery/Gallery6.jpg</url>
+                <title>Black Clover</title>
+                <filesize>78063</filesize>
+            </Image>
+            <Image>
+                <url>C:/Users/ha/Desktop/project 1/images/Gallery/Gallery7.jpg</url>
+                <title>Other Anime</title>
+                <filesize>99134</filesize>
+            </Image>
+        </Images>
+    </Gallery>
+</VisualLightBox>
